df3f3cf0b6305b0af930af72873496f04a900754
moranidan <moranidan1233@gmail.com>
2025-06-25 15:40:48 +0300

    support in unlimited amount of sh commands input

--- shtrudel/shtrudel.c
+++ shtrudel/shtrudel.c
@@ -3,17 +3,8 @@
 int main(int argc, char *argv[]) {
     CHECK(argc >= 2, "missing sh command");
 
-    int shtrudel_idx = get_shtrudel_idx(argc, argv);
-
-    if(shtrudel_idx == 0) { //No shtrudel recived , exec one sh command
-        execv(argv[1], &argv[1]);
-        CHECK(false, "Failed to exec"); //If this line runs it means execv failed
-    }
-    else {
-        argv[shtrudel_idx] = NULL; //Set "@" to NULL - to seperate between the commands"
-        int ret = execv_w_shtrudel(shtrudel_idx, argv); //Preform two command with pipe
-        CHECK(ret ==0, "Failed to exec with shtrudel");
-    }
+    int ret = check_shtrudel_and_execv(first_command_idx, argc, argv);
+    CHECK(ret ==0, "Failed in check_shtrudel_and_execv func");
 
     return 0;
 error:
@@ -22,14 +13,35 @@ error:
 
 int get_shtrudel_idx(int argc, char *argv[]) {
     for (int i =0; i < argc; i++) {
+        if(argv[i] == NULL) { //Skip the already found "@" that have been turn into NULL
+            continue;
+        }
         if(strncmp(argv[i], "@", strlen(argv[i])) == 0) { //Get shtrudel idx
+            argv[i] = NULL; //Set "@" to NULL - to seperate between the commands after in execv"
             return i;
         }
     }
     return 0; //Zero is the error return value beacuse argv[0] is this function exec path and cant be @.
 }
 
-int execv_w_shtrudel(int shtrudel_idx, char *argv[]){
+int check_shtrudel_and_execv(int before_shtrudel_command_idx, int argc, char *argv[]) {
+    int shtrudel_idx = get_shtrudel_idx(argc, argv); //Get shturdel_idx
+
+    if(shtrudel_idx == 0) { //No shtrudel left or recived , exec sh command
+        execv(argv[before_shtrudel_command_idx], &argv[before_shtrudel_command_idx]);
+        CHECK(false, "Failed to exec"); //If this line runs it means execv failed
+    }
+    else {
+        int ret = execv_w_shtrudel(before_shtrudel_command_idx, shtrudel_idx, argc, argv); //Preform two command with pipe
+        CHECK(ret ==0, "Failed to exec with shtrudel");
+    }
+
+    return 0;
+error:
+    return -1;
+}
+
+int execv_w_shtrudel(int before_shtrudel_command_idx, int shtrudel_idx, int argc, char *argv[]) {
     int pipefd[2];
     CHECK(pipe(pipefd) == 0, "Failed to do pipe");
     
@@ -40,14 +52,14 @@ int execv_w_shtrudel(int shtrudel_idx, char *argv[]){
     if(pid ==0) { //In child process, do second command, after shtrudel
         close(pipefd[1]);
         dup2(pipefd[0], STDIN_FILENO); //Set input fd to the pipe input
-        execv(argv[shtrudel_idx+1], &argv[shtrudel_idx+1]);
-        CHECK(false, "Failed to exec second command"); //If this line runs it means execv failed
+        int ret = check_shtrudel_and_execv(shtrudel_idx+1, argc, argv); //Run the program again - recursion
+        CHECK(ret == 0, "Failed in check_shtrudel_and_execv func");
     }
     else { //In parent process, do first command, before shtrudel
         close(pipefd[0]);
         dup2(pipefd[1], STDOUT_FILENO); //Set output fd to the pipe output
-        execv(argv[1], &argv[1]);
-        CHECK(false, "Failed to exec first command"); //If this line runs it means execv failed
+        execv(argv[before_shtrudel_command_idx], &argv[before_shtrudel_command_idx]);
+        CHECK(false, "Failed to exec command"); //If this line runs it means execv failed
     }
 
     return 0;
--- shtrudel/shtrudel.h
+++ shtrudel/shtrudel.h
@@ -7,6 +7,8 @@
 #include <unistd.h>
 #include <stdbool.h>
 
+#define first_command_idx 1
+
 #define CHECK(val, log_msg) \
     do { \
         if (!(val)) { \
@@ -26,9 +28,23 @@ get_shtrudel_idx - This function get the "@" idx inside argv[]
 int get_shtrudel_idx(int argc, char *argv[]);
 
 /*
-execv_w_shtrudel - This function execv the commands given in argv, replacing the "@" in the pipe funcuality
-            input - (int) shtrudel_idx - the idx of "@" in argv[]
+check_shtrudel_and_execv - This function checks if there is another "@" in argv, if there isnt it run the sh command. if there is it calls the execv_w_shtrudel
+                            this function being called in recursion. 
+            input - (int) before_shtrudel_command_idx - the idx of first command to run, the one before the "@" if there is a "@". 
+                    (int) argc - the number of elements in the program running line
                     (char *[]) argv - the list of argument recived in the program running line
             output -  (int) 0 in case of sucess, -1 otherwise.
 */
-int execv_w_shtrudel(int shtrudel_idx, char *argv[]);
+int check_shtrudel_and_execv(int before_shtrudel_command_idx, int argc, char *argv[]);
+
+/*
+execv_w_shtrudel - This function execv the commands given in argv, replacing the "@" in the pipe funcuality, 
+                    the before shtrudel command run in the father process and the after shtrudel command that run in the child proccess 
+                    looks for another "@" in the argv using get_shtrudel_idx and calls check_shtrudel_and_execv again.
+            input - (int) before_shtrudel_command_idx - the idx of first command to run, the one before the "@" if there is a "@". 
+                    (int) shtrudel_idx - the idx of "@" in argv[]
+                    (int) argc - the number of elements in the program running line
+                    (char *[]) argv - the list of argument recived in the program running line
+            output -  (int) 0 in case of sucess, -1 otherwise.
+*/
+int execv_w_shtrudel(int before_shtrudel_command_idx, int shtrudel_idx, int argc, char *argv[]);
